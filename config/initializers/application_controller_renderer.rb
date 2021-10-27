66c2f33dbba1b76759815f2bea1e4406fa7ad996
Aya1996syuger <ayaka.coco87@gmail.com>
2021-10-27 00:16:33 +0000

    post

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c89bda504a441561c8f154cddc4a1cf914c7af8039390249a5e2fba31030bdcadcde9e99fe5145b2ebdfc06e016ca24b9f68a6f8f129639f72b30dfd28f89be1'
